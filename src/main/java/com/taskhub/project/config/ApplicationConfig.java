c074423e6c303b3dc21c7c8ba8b444e56b6e04ad
Việt Hưng Nguyễn <nvh202sua@gmail.com>
2024-04-08 15:42:59 +0700

    add logging

--- src/main/java/com/taskhub/project/config/ApplicationConfig.java
+++ src/main/java/com/taskhub/project/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @RequiredArgsConstructor
